883e1ab920b66a9b63a09c491e42ea07b1803fda
pwangy <blipsandclicks@gmail.com>
2024-01-29 15:49:00 -0800

    calculatesFarePrice passes

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ function distanceTravelledInFeet(n1, n2) {
     return (n2-n1)*264
     }
 }
+
+function calculatesFarePrice(n1, n2) {
+    if (distanceTravelledInFeet(n1, n2) < 400) {
+        return 0
+    } else if ((distanceTravelledInFeet(n1, n2) >= 401 && distanceTravelledInFeet(n1, n2) <= 2000)) {
+        const getFare = (distanceTravelledInFeet(n1, n2) - 400) * 0.02
+        return getFare
+    } else if ((distanceTravelledInFeet(n1, n2) >= 2001 && (distanceTravelledInFeet(n1, n2) <= 2500))){
+        return 25
+    } else {
+    return 'cannot travel that far'
+    }
+}
